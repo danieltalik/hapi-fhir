731043849bfc2213c50d3f155cd1cedd6f0a5d70
dtalik <danieltalik@gmail.com>
2020-06-08 14:25:01 -0400

    Adding Auth Server to repo

---
+++ AuthorizationServerApplication/src/main/java/com/g2o/hapi/AuthorizationServerApplication/UserController.java
@@ -0,0 +1,13 @@
+package com.g2o.hapi.AuthorizationServerApplication;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.security.Principal;
+
+@RestController
+public class UserController {
+    @GetMapping("/user/me")
+    public Principal user(Principal principal) {
+        return principal;
+    }
+}
